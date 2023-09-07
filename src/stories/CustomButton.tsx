e5119b818bcc41f5f1782a2ee3a8aefd72ecc40c
inn3rGames <hi@inn3r.com>
2023-09-07 15:15:30 +0300

    Create custom button structure

--- src/stories/CustomButton.tsx
+++ src/stories/CustomButton.tsx
@@ -1,44 +1,51 @@
-import React from 'react';
-import { Button } from 'antd';
-import { FireFilled } from '@ant-design/icons';
-import { PlusSquareOutlined } from '@ant-design/icons';
+import React from "react";
+import { Button } from "antd";
+import { FireFilled } from "@ant-design/icons";
+import { PlusOutlined } from "@ant-design/icons";
 
 interface CustomButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary?: boolean;
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    /**
+     * How large should the button be?
+     */
+    size?: "small" | "medium" | "large";
+    /**
+     * Button contents
+     */
+    label: string;
+    /**
+     * Optional click handler
+     */
+    onClick?: () => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const CustomButton = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+    primary = false,
+    size = "medium",
+    backgroundColor,
+    label,
+    ...props
 }: CustomButtonProps) => {
-  return (
-
-    <Button {...props}><FireFilled />button / 02<FireFilled /></Button>
-
-  );
+    return (
+        <div>
+            <Button {...props}>
+                <FireFilled />
+                button / 02
+                <FireFilled />
+            </Button>{" "}
+            <Button>
+                <PlusOutlined />
+            </Button>
+        </div>
+    );
 };
